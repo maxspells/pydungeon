18edd3f5196fe8aa75a27934df8c4090f9ea77d4
Maxspells <cory.jnson@gmail.com>
2023-02-14 17:46:04 -0500

    basic framework pygame window

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("helloworld")
+import pygame
+pygame.init
+screen = pygame.display.set_mode((800,400))
+pygame.display.set_caption('Vidya Gaem')
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+    pygame.display.update()
