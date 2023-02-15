24f8373cd84fd06e1b42a3ce1814c6fea2e6daee
Maxspells <cory.jnson@gmail.com>
2023-02-14 20:30:52 -0500

    tutorial stuff on adding images to pygame screen

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import pygame
-pygame.init
-screen = pygame.display.set_mode((800,400))
+from sys import exit
+pygame.init()
+pygame.font.init()
+screen = pygame.display.set_mode((760,575))
 pygame.display.set_caption('Vidya Gaem')
+clock = pygame.time.Clock()
+test_font = pygame.font.Font(None,50)
+
+sky_surface = pygame.image.load("gfx/sky.png")
+text_surface = test_font.render('WuTang Clan',True,'Green')
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+            exit()
+    screen.blit(sky_surface,(0,0))
+    screen.blit(text_surface,(20,20))
+
     pygame.display.update()
+    clock.tick(60)
