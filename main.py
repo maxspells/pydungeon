7f1144ad74c4b1db81157342c6997784c1b471c9
Maxspells <cory.jnson@gmail.com>
2023-02-15 22:03:25 -0500

    this is now a pokemon themed game

--- main.py
+++ main.py
@@ -1,35 +1,41 @@
 import pygame
+import random
 from sys import exit
 pygame.init()
 pygame.font.init()
-screen = pygame.display.set_mode((760,575))
+screen = pygame.display.set_mode((800,500))
 pygame.display.set_caption('Vidya Gaem')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font(None,50)
-sky_surface = pygame.image.load("gfx/sky/mtsky.png").convert()
-bird1 = pygame.image.load("gfx/sprite/bird1.png")
-bird2 = pygame.image.load("gfx/sprite/bird2.png")
-bird3 = pygame.image.load("gfx/sprite/bird3.png")
-text_surface = test_font.render('WuTang Clan',True,'Green')
-bird_sprite = [bird1,bird2,bird3]
-animate = 0
-bird_x = -200
-bird_y = 100
+sky_surface = pygame.image.load("gfx/field.jpg").convert()
+pikachu = pygame.image.load("gfx/pikachu.png")
+pika_rect = pikachu.get_rect(midbottom = (400,400))
+pokeball = pygame.image.load("gfx/pokeball.png")
+pos = [400,400]
+ball_rect = pokeball.get_rect(midbottom = (pos))
+counter = 0
+text_surface = test_font.render(str(counter),True,'Black')
+pikachu_x = 200
+pikachu_y = 440
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    if animate == 2:
-        animate = 0
-    else:
-        animate += 1
-    screen.blit(sky_surface,(-300,-300))
+        if event.type == pygame.MOUSEBUTTONUP:
+            pos = pygame.mouse.get_pos()
+            ball_rect.midbottom = pos
+            print(pos)
+    screen.blit(sky_surface,(-300,-500))
     screen.blit(text_surface,(20,20))
-    screen.blit(bird_sprite[animate],(bird_x,bird_y))
-    bird_x += 10
-    if bird_x == 800:
-        bird_x = -400
+    screen.blit(pikachu,pika_rect)
+    screen.blit(pokeball,ball_rect)
+    if pika_rect.colliderect(ball_rect):
+        pikachu_x = random.randint(100,700)
+        pikachu_y = random.randint(350,520)
+        pika_rect.midbottom = pikachu_x,pikachu_y
+        counter += 1
+        text_surface = test_font.render(str(counter),True,'Black')
     pygame.display.update()
     clock.tick(10)
